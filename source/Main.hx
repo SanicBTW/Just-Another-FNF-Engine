3b747c5541090fe135bb31a4a470469e20794506
SanicBTW <58010536+SanicBTW@users.noreply.github.com>
2023-03-20 00:59:10 +0100

    Added a crash handler

--- source/Main.hx
+++ source/Main.hx
@@ -18,6 +18,17 @@ import openfl.events.Event;
 #if cpp
 import cpp.NativeGc;
 #end
+#if windows
+import haxe.CallStack;
+import haxe.io.Path;
+import lime.app.Application;
+import openfl.events.UncaughtErrorEvent;
+import sys.FileSystem;
+import sys.io.File;
+import sys.io.Process;
+
+using StringTools;
+#end
 
 class Main extends Sprite
 {
@@ -135,5 +146,49 @@ class Main extends Sprite
 			if (volumeTray != null && volumeTray.active)
 				volumeTray.update();
 		});
+
+		#if window
+		Lib.current.loaderInfo.uncaughtErrorEvents.addEventListener(UncaughtErrorEvent.UNCAUGHT_ERROR, onCrash);
+		#end
 	}
+
+	// Code was entirely made by sqirra-rng for their fnf engine named "Izzy Engine", big props to them!!!
+	#if windows
+	function onCrash(e:UncaughtErrorEvent):Void
+	{
+		var errMsg:String = "";
+		var path:String;
+		var callStack:Array<StackItem> = CallStack.exceptionStack(true);
+		var dateNow:String = Date.now().toString();
+
+		dateNow = dateNow.replace(" ", "_");
+		dateNow = dateNow.replace(":", "'");
+
+		path = "./crash/" + "FRB2_" + dateNow + ".txt";
+
+		for (stackItem in callStack)
+		{
+			switch (stackItem)
+			{
+				case FilePos(s, file, line, column):
+					errMsg += file + " (line " + line + ")\n";
+				default:
+					Sys.println(stackItem);
+			}
+		}
+
+		errMsg += "\nUncaught Error: " + e.error + "\nPlease report this error to the sanco#8424\n\n> Crash Handler written by: sqirra-rng";
+
+		if (!FileSystem.exists("./crash/"))
+			FileSystem.createDirectory("./crash/");
+
+		File.saveContent(path, errMsg + "\n");
+
+		Sys.println(errMsg);
+		Sys.println("Crash dump saved in " + Path.normalize(path));
+
+		Application.current.window.alert(errMsg, "Error!");
+		Sys.exit(1);
+	}
+	#end
 }
