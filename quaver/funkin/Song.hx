47164c04892b2767567afaa2d80972c8c2684120
SanicBTW <58010536+SanicBTW@users.noreply.github.com>
2023-09-27 01:16:52 +0200

    Working on section metadata Setting up metadata on parsing the chart, if it works then its gonna be really useful.

--- quaver/funkin/Song.hx
+++ quaver/funkin/Song.hx
@@ -4,14 +4,31 @@ import haxe.Json;
 
 using StringTools;
 
+enum LineType
+{
+	HEADER;
+	BODY;
+}
+
+typedef SectionLine =
+{
+	var type:LineType;
+	var time:Float;
+}
+
 typedef SwagSection =
 {
 	var sectionNotes:Array<Dynamic>;
 	var sectionBeats:Null<Int>;
 	var lengthInSteps:Int;
 	var mustHitSection:Bool;
-	var bpm:Float;
+	var bpm:Null<Float>;
 	var changeBPM:Bool;
+
+	// section info shit for section creation my bad
+	var startTime:Float; // based on step crochets no funky elapsed shit sorry
+	var endTime:Float;
+	var lines:Array<SectionLine>;
 }
 
 typedef SwagSong =
@@ -33,6 +50,30 @@ class Song
 			rawInput = rawInput.substr(0, rawInput.length - 1);
 		}
 
-		return cast Json.parse(rawInput).song;
+		// Recalculate section timings based on the section bpm
+		var swagShit:SwagSong = cast Json.parse(rawInput).song;
+		var bpm:Float = swagShit.bpm;
+
+		for (section in swagShit.notes)
+		{
+			var index:Int = swagShit.notes.indexOf(section);
+			if (section.changeBPM)
+				bpm = section.bpm;
+
+			// 4 beats of the section * 4 default beats = 16 steps
+			var steps:Int = (section.sectionBeats != null) ? (section.sectionBeats * 4) : section.lengthInSteps;
+			// 16 steps / 4 default beats = 4 beats
+			var beats:Int = (section.sectionBeats != null) ? section.sectionBeats : Math.floor((section.lengthInSteps / 4));
+
+			var crochet:Float = (60 / bpm) * 1000;
+			var stepCrochet:Float = (crochet / beats);
+
+			// So the uhhhhh section must always need a header section including 4 body lines
+			section.startTime = (index * stepCrochet);
+			section.endTime = (section.startTime * steps);
+			trace(bpm, section.startTime, section.endTime);
+		}
+
+		return swagShit;
 	}
 }
